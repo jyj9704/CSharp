592b73d2020c25e509e6af82e5a51e034051fd8d
YEJI JO <jyj9704@naver.com>
2022-05-23 17:44:24 +0900

    Initial commit

---
+++ CSharp/DataTypes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+
+            // Value타입 - 숫자, boolean, 문자타입, null지정 불가능
+            short sh = 100;
+            int i = 100;
+            long l = 10000L; // 부호가 있는 정수
+            ushort us = 111;
+            uint ui = 110U;
+            ulong ul = 1000UL; // 부호가 없는 정수 앞에 u사용
+
+            float f = 3.14f; // 32bit
+            double d = 3.141592d; // 64bit
+            decimal dd = 3.14m; // 128bit 
+
+            // Reference타입 - 문자열, null지정 가능 
+            string s = "Hello";
+
+            char ch = 'A'; // 문자 한개
+
+            byte by = 0 * 46;
+
+            object o = 123; //모든 데이타를 포함할 수 있는 타입
+
+            int? ix = null; // int? 사용ix가 null값을 가질 수 있음
+            if (ix == null)
+            {
+                Console.WriteLine("ix is null");
+            }
+            else
+            {
+                Console.WriteLine(ix.Value);
+            }
+
+        }
+    }
+}
---
+++ CSharp/Save1to100.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Save1to100
+    {
+        static void Main(string[] args)
+        {
+            // 1부터 100까지를 변수에 저장
+            /* n[0] = 1;
+               n[1] = 2;
+               // ...
+               n[99] = 100; // 반복문 사용시 단순화 가능
+            */
+
+            int[] n = new int[100];
+            for (int i = 0; i < n.Length; i++) // 100미만시 이 루프 계속 돎
+            {
+                n[i] = i + 1;
+            }
+
+            int x = n[10]; // x에11번째에 숫자 할당!
+
+            // 배열을 함수(메서드)에 전달
+            PrintArray(n);
+        }
+        static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
